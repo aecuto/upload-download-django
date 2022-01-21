ae78b820dec8cd408c9147df7c36d67740d983b5
aecuto <kampee.expertly01@gmail.com>
2022-01-22 02:18:20 +0700

    added upload image

--- upload/views.py
+++ upload/views.py
@@ -1,4 +1,5 @@
 
+from datetime import date
 from django.views.generic import CreateView, DetailView
 
 from django.http import HttpResponse
@@ -6,20 +7,35 @@ from django.http import HttpResponse
 from .form import UploadForm
 from .models import Upload
 
+from django.urls import reverse_lazy
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-    
-
 class UploadPage(CreateView):
     model = Upload
     form_class = UploadForm
-
     # TODO:
     # 1) Convert expire_duration to expire_date
     # 2) Upload and save
     # 3) Generate download and delete link
+    def form_valid(self, form):
+        form.instance.expire_date = date.today()
+        return super().form_valid(form)
+#     new_form = UploadForm(request.POST)
+
+# # Create, but don't save the new author instance.
+# >>> new_author = f.save(commit=False)
+
+# # Modify the author in some way.
+# >>> new_author.some_field = 'some_value'
+
+# # Save the new instance.
+# >>> new_author.save()
+
+# # Now, save the many-to-many data for the form.
+# >>> f.save_m2m()
 
 
 class Download(DetailView):
