99629211260dfb4ce243fdd8ac6c756e5e0ffdde
aecuto <kampee.expertly01@gmail.com>
2022-01-23 02:02:18 +0700

    update utils

--- upload/utils.py
+++ upload/utils.py
@@ -1,20 +1,19 @@
 import os
-import random
-import string
+from time import time
 
 BASE_FILE_PATH = 'files'
 
-def uniq_id(size=6, chars=string.ascii_uppercase + string.digits):
-     return ''.join(random.choice(chars) for _ in range(size))
+def get_upload_path(file_name):
+    return os.path.join(BASE_FILE_PATH,file_name)
 
 def handle_uploaded_file(f):
-    filename = uniq_id() + '-' + f.name
-    path = os.path.join(BASE_FILE_PATH,filename)
+    filename = str(int(time())) +'.'+ f.name.split(".")[1]
+    upload_path = get_upload_path(filename)
 
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
+    if not os.path.exists(os.path.dirname(upload_path)):
+        os.makedirs(os.path.dirname(upload_path))
 
-    with open(path, 'wb+') as destination:
+    with open(upload_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
@@ -23,9 +22,6 @@ def handle_uploaded_file(f):
 def handle_delete_file(path):
    if os.path.isfile(path):
        os.remove(path)
-
-def get_upload_path(file_name):
-    return os.path.join(BASE_FILE_PATH,file_name)
 
 def validate_file_size(file):
     filesize= file.size
