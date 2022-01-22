1b80b790f0617a7c616ef7fc5bd1623dfada6631
aecuto <kampee.expertly01@gmail.com>
2022-01-22 14:05:25 +0700

    add download file

--- upload/utils.py
+++ upload/utils.py
@@ -1,11 +1,13 @@
 import os
 
 def handle_uploaded_file(f):
-  filename = os.path.join('files',f.name)
+    filename = os.path.join('files',f.name)
 
-  if not os.path.exists(os.path.dirname(filename)):
-    os.makedirs(os.path.dirname(filename))
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
 
-  with open(filename, 'wb+') as destination:
-    for chunk in f.chunks():
-      destination.write(chunk)
+    with open(filename, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+    return filename
--- upload/views.py
+++ upload/views.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import os
 from django.views.generic import CreateView, DetailView
 
-from django.http import HttpResponse
+from django.http import FileResponse, HttpResponse
 
 from upload.utils import handle_uploaded_file
 
@@ -24,10 +24,12 @@ class UploadPage(CreateView):
 
     def form_valid(self, form):
         duration = self.request.POST.get('expire_duration')
-        form.instance.expire_date = datetime.now() + timedelta(seconds=int(duration))
-
         file = self.request.FILES['file']
-        handle_uploaded_file(file)
+
+        upload_path = handle_uploaded_file(file)
+
+        form.instance.expire_date = datetime.now() + timedelta(seconds=int(duration))
+        form.instance.upload_path = upload_path
 
         return super().form_valid(form)
 
@@ -40,11 +42,15 @@ class Download(DetailView):
 
     def post(self, *args, **kwargs):
         self.object = self.get_object()
+        print(self.object.upload_path)
 
         if self.object.password is not None:
             if self.request.POST.get("password") != "password":
                 return HttpResponse("invalid password")
 
+
+        response = FileResponse(open(self.object.upload_path, 'rb'))
+        return response
         return HttpResponse("todo")
 
         # TODO:
