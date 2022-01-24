92b0e243079d178bf25aab5c990b04a2e701ec71
aecuto <kampee.expertly01@gmail.com>
2022-01-24 18:03:00 +0700

    add bg process

--- upload/apps.py
+++ upload/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
+from .scheduler.run_script import run_continuously
 
 class UploadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'upload'
+    def ready(self):
+        run_continuously()
 
 
 
