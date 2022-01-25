c99dc68e4a4a54c6aa5b586e347a5f87ac7165b1
aecuto <kampee.expertly01@gmail.com>
2022-01-25 11:55:42 +0700

    update id to uuid

--- upload/models.py
+++ upload/models.py
@@ -1,9 +1,11 @@
+from uuid import uuid4
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 
 class Upload(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     password = models.CharField(max_length=255, blank=True, null=True)
     max_downloads = models.IntegerField()
     expire_date = models.DateTimeField()
